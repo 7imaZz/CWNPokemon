ff18881e056b5150874d93d9f405708af64da001
7ima <abdelrahimelshorbgy@gmail.com>
2020-10-11 22:53:45 +0200

    Hilt Dagger & Retrofit

---
+++ app/src/main/java/com/example/cwnpokemon/network/PokemonApi.java
@@ -0,0 +1,12 @@
+package com.example.cwnpokemon.network;
+
+import com.example.cwnpokemon.pojo.PokemonResponse;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+public interface PokemonApi {
+
+    @GET("pokemon")
+    Observable<PokemonResponse> getPokes();
+}
